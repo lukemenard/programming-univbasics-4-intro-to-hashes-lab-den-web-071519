6224365d72be26de432d165354a4e1729c2b3000
Luke Menard <lukemenard@gmail.com>
2019-07-05 22:17:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,10 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash[:name] = "Maple"
+  my_hash[:breed] = "German Shepherd"
 end
 
 def pioneer
